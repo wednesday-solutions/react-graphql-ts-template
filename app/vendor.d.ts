08071e2dd31e9472fd8ca0f756c631ceb264cf4a
Anurag-Wednesday <anurag@wednesday.is>
2022-03-22 13:32:46 +0530

    fixed issue with lint

--- app/vendor.d.ts
+++ app/vendor.d.ts
@@ -11,7 +11,7 @@ declare module 'redux-persist-transform-immutable' {
   import { Transform } from 'redux-persist/es/types';
 
   interface Config {
-    records: Record[];
+    records: Record<Object>;
   }
 
   export default function <HSS, ESS, S = any, RS = any>(config?: Config): Transform<HSS, ESS, S, RS>;

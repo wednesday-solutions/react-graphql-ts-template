eaecd7ded3884cba75e036ceea06f497eda2f619
Prabhat Singh <prabhat@wednesday.is>
2022-10-13 15:34:33 +0530

    refactor: remove unnecessary changes

--- app/services/apiUtils.ts
+++ app/services/apiUtils.ts
@@ -1,4 +1,4 @@
 import { generateApiClient } from '@app/utils/apiUtils';
 const ituneApi = generateApiClient('itune');
 
-export const getItune = (param: any) => ituneApi.get(`/search?term=${param}&limit=10&offset=20`);
+export const getItune = (param: any) => ituneApi.get(`/search?term=${param}`);

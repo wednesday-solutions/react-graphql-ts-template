1ec047aa1fa6354c93dda552092a7fc33d83ce0b
Anas WS <anas@wednesday.is>
2024-03-08 11:26:38 +0530

    fix: mockData added in separate file

---
+++ app/containers/HomeContainer/tests/mockData.js
@@ -0,0 +1,112 @@
+export const LAUNCHES = [
+  {
+    id: '1',
+    missionName: 'Thaicom 6',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/Thaicom_6'
+    },
+    launchDateUtc: '2014-01-06T14:06:00-04:00',
+    launchDateUnix: 1389031560
+  },
+  {
+    id: '1',
+    missionName: 'AsiaSat 6',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/AsiaSat_6'
+    },
+    launchDateUtc: '2014-09-07T01:00:00-04:00',
+    launchDateUnix: 1410066000
+  },
+  {
+    id: '1',
+    missionName: 'OG-2 Mission 2',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/Falcon_9_flight_20'
+    },
+    launchDateUtc: '2015-12-22T21:29:00-04:00',
+    launchDateUnix: 1450747740
+  },
+  {
+    id: '1',
+    missionName: 'FalconSat',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/DemoSat'
+    },
+    launchDateUtc: '2006-03-25T10:30:00+12:00',
+    launchDateUnix: 1143239400
+  },
+  {
+    id: '1',
+    missionName: 'CRS-1',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/SpaceX_CRS-1'
+    },
+    launchDateUtc: '2012-10-08T20:35:00-04:00',
+    launchDateUnix: 1349656500
+  },
+  {
+    id: '1',
+    missionName: 'CASSIOPE',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/CASSIOPE'
+    },
+    launchDateUtc: '2013-09-29T09:00:00-07:00',
+    launchDateUnix: 1380470400
+  },
+  {
+    id: '1',
+    missionName: 'ABS-3A / Eutelsat 115W B',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/ABS-3A'
+    },
+    launchDateUtc: '2015-03-02T23:50:00-04:00',
+    launchDateUnix: 1425268200
+  },
+  {
+    id: '1',
+    missionName: 'COTS 1',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/SpaceX_COTS_Demo_Flight_1'
+    },
+    launchDateUtc: '2010-12-08T11:43:00-04:00',
+    launchDateUnix: 1291822980
+  },
+  {
+    id: '1',
+    missionName: 'TürkmenÄlem 52°E / MonacoSAT',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/T%C3%BCrkmen%C3%84lem_52%C2%B0E_/_MonacoSAT'
+    },
+    launchDateUtc: '2015-04-27T19:03:00-04:00',
+    launchDateUnix: 1430175780
+  },
+  {
+    id: '1',
+    missionName: 'CRS-11',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/SpaceX_CRS-11'
+    },
+    launchDateUtc: '2017-06-03T17:07:00-04:00',
+    launchDateUnix: 1496524020
+  },
+  {
+    id: '1',
+    missionName: 'Iridium NEXT Mission 1',
+    links: {
+      flickrImages: ['image1', 'image2'],
+      wikipedia: 'https://en.wikipedia.org/wiki/Iridium_satellite_constellation#Next-generation_constellation'
+    },
+    launchDateUtc: '2017-01-14T10:54:00-07:00',
+    launchDateUnix: 1484416440
+  }
+];
